36d2e9cbd603e6622d3886411797c60c562e81dc
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-04 21:26:22 +0530

    Some more boilerplate

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 
 build:
-	cargo build --target x86_64-catalyst.json -Z build-std=core,compiler_builtins -Z build-std-features=compiler-builtins-mem
+	cargo bootimage --target x86_64-catalyst.json -Z build-std=core,compiler_builtins -Z build-std-features=compiler-builtins-mem
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,20 @@ mod utils;
 use utils::panic::panic;
 // ===================
 
+static HELLO: &[u8] = b"Hello World!";
+
 /// # Start Function
 /// Entry-Point for the OS.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+  let vga_buffer = 0xb8000 as *mut u8;
+
+  for (i, &byte) in HELLO.iter().enumerate() {
+    unsafe {
+      *vga_buffer.offset(i as isize * 2) = byte;
+      *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    }
+  }
+
   loop {}
 }
