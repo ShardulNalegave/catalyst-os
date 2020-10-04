2d840fd7b10455457c086726b409a49fed75b653
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-04 22:46:21 +0530

    Work on Color and ColorCode in VGA module

--- src/vga/color.rs
+++ src/vga/color.rs
@@ -1,4 +1,35 @@
 
-pub enum Color {}
+/// # Color enum
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
 
-pub struct ColorCode(u8);
+/// # Color Code
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    /// ## Constructor
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
