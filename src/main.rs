abf3b1b24fabd0e17df2d9028536e02e9ba87b77
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-07 18:53:31 +0530

    Removed everything related to tests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::tests::runner)]
-#![reexport_test_harness_main = "test_main"]
 
 /// Utils module
 pub mod utils;
 
 /// VGA module
 pub mod vga;
-
-/// Serial module
-pub mod serial;
-
-/// Tests module
-#[cfg(test)]
-pub mod tests;
 
 // ===== Imports =====
 #[allow(unused_imports)]
@@ -29,9 +19,6 @@ pub extern "C" fn _start() -> ! {
   vga_println!("Hello, World!");
   vga_print!("How are you?");
   vga_println!(" I am fine!");
-
-  #[cfg(test)]
-  test_main();
 
   loop {}
 }
