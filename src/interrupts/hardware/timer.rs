d64e796d90b725d10398d2dce5e6c9425b42ef3b
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-08 21:57:40 +0530

    Working on interrupts module

--- src/interrupts/hardware/timer.rs
+++ src/interrupts/hardware/timer.rs
@@ -1,10 +1,16 @@
 
 // ===== Imports =====
 use crate::*;
+use crate::interrupts::hardware::{PICS, PICSInterruptIndex};
 use x86_64::structures::idt::InterruptStackFrame;
 // ===================
 
 pub extern "x86-interrupt" fn timer_interrupt_handler(
     _stack_frame: &mut InterruptStackFrame) {
-    vga_println!("timer interrupt.");
+    vga_print!("timer.");
+
+    // Notify that the interrupt is now processed
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(PICSInterruptIndex::Timer.as_u8());
+    }
 }
