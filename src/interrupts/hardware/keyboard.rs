cbcbdb5e2332ba78551d8f8e13b35742f75258c9
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-13 21:59:42 +0530

    Working on Keyboard interrupts

--- src/interrupts/hardware/keyboard.rs
+++ src/interrupts/hardware/keyboard.rs
@@ -3,10 +3,29 @@
 use crate::*;
 use crate::interrupts::hardware::{PICS, PICSInterruptIndex};
 use x86_64::structures::idt::InterruptStackFrame;
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+use spin::Mutex;
+use x86_64::instructions::port::Port;
+use lazy_static::lazy_static;
 // ===================
 
+lazy_static! {
+    /// # Keyboard
+    /// A static reference to the Keyboard struct.
+    static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+        Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1,
+        HandleControl::Ignore)
+    );
+}
+
 pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
-    vga_print!("{}", get_scancode());
+    match get_key() {
+        Some(key) => match key {
+            DecodedKey::Unicode(ch) => vga_print!("{}", ch),
+            DecodedKey::RawKey(key) => vga_print!("{:?}", key),
+        },
+        None => (),
+    }
 
     // Notify that the interrupt is now processed
     unsafe {
@@ -14,9 +33,15 @@ pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Inte
     }
 }
 
-/// # Get Scancode
-/// Scans the keyboard port to receive a scancode i.e. keycode basically.
-fn get_scancode() -> u8 {
-    use x86_64::instructions::port::Port;
-    unsafe { Port::new(0x60).read() }
+/// # Get Key
+/// Returns the PC-Keyboard Decoded Key.
+fn get_key() -> Option<DecodedKey> {
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+    let scancode = unsafe { port.read() };
+
+    match keyboard.add_byte(scancode) {
+        Ok(Some(key_event)) => keyboard.process_keyevent(key_event),
+        _ => None,
+    }
 }
