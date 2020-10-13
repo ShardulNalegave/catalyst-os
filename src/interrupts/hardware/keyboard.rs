9ca2eaf9becbf2d3e08057fec6390a7a2c21583e
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-13 21:40:15 +0530

    Added 'pc-keyboard' dependency and some other stuff

--- src/interrupts/hardware/keyboard.rs
+++ src/interrupts/hardware/keyboard.rs
@@ -6,10 +6,17 @@ use x86_64::structures::idt::InterruptStackFrame;
 // ===================
 
 pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
-    vga_print!("k");
+    vga_print!("{}", get_scancode());
 
     // Notify that the interrupt is now processed
     unsafe {
         PICS.lock().notify_end_of_interrupt(PICSInterruptIndex::Keyboard.as_u8());
     }
+}
+
+/// # Get Scancode
+/// Scans the keyboard port to receive a scancode i.e. keycode basically.
+fn get_scancode() -> u8 {
+    use x86_64::instructions::port::Port;
+    unsafe { Port::new(0x60).read() }
 }
