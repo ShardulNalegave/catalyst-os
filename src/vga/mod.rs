d7693f3f666ef3d05e84ade9e08cc616a7c83ee4
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-13 21:04:27 +0530

    Fixed an important bug!

--- src/vga/mod.rs
+++ src/vga/mod.rs
@@ -105,5 +105,7 @@ impl fmt::Write for VGAWriter {
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    WRITER.lock().write_fmt(args).unwrap();
+    x86_64::instructions::interrupts::without_interrupts(|| {
+        WRITER.lock().write_fmt(args).unwrap();
+    });
 }
