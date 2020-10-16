903f2cc103eddf75f0d3a6c08dfb0f9c4eeeb51f
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-10-16 21:13:22 +0530

    Working on Cargo config

--- shared/src/tests/mod.rs
+++ shared/src/tests/mod.rs
@@ -2,6 +2,7 @@
 /// # Tests Runner
 /// Runs all the defined tests.
 pub fn runner(tests: &[&dyn Fn()]) {
+    vga::println!("Running {} tests.", tests.len());
     for test in tests {
         test();
     }
